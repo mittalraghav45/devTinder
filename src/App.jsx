6d06d4a42a6b6345b7dcc5a2196157de3baee62c
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-16 00:54:56 +0000

    daisyUi installed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ function App() {
 
   return (
     <>
-       <h1>Hii</h1>
+<button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title</h2>
+    <p>Beautiful DaisyUI components!</p>
+  </div>
+</div>
+
     </>
   )
 }
